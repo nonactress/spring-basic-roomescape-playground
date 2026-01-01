8f084a78246c6f90e858d99c6ff378eb6079b2da
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-01-01 18:06:07 +0900

    feat : 예외 처리

---
+++ src/main/java/roomescape/advice/ErrorResponse.java
@@ -0,0 +1,4 @@
+package roomescape.advice;
+
+public record ErrorResponse(String message) {
+}
---
+++ src/main/java/roomescape/advice/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package roomescape.advice;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(RuntimeException e) {
+        return ResponseEntity.status(404)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse("오류가 발생했습니다."));
+    }
+}
