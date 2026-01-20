52a80f25f13405cd9beef335c251adc6dddad402
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-01-21 02:31:52 +0900

    fix : 로그인 관련 401 상태코드로 변경

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -2,6 +2,7 @@ package roomescape.member;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import roomescape.exception.AuthenticationException;
 import roomescape.infrastructure.JwtTokenProvider;
 
 @Service
@@ -44,7 +45,7 @@ public class MemberService {
                         memberRequest.getEmail(),
                         memberRequest.getPassword()
                 )
-                .orElseThrow(() -> new RuntimeException("이메일 또는 비밀번호가 일치하지 않습니다."));
+                .orElseThrow(() -> new AuthenticationException("이메일 또는 비밀번호가 존재하지 않거나 일치하지 않습니다."));
 
         return jwtTokenProvider.createToken(member.getEmail());
     }
